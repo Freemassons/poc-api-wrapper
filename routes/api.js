faafd98443273fb6a740febc47aae8cb9369f636
Chris Figgins <figginsc@gmail.com>
2018-11-01 18:59:50 -0400

    Quick change

--- routes/api.js
+++ routes/api.js
@@ -19,7 +19,7 @@ router.get('/', function(req, res, next) {
     // The whole response has been received. Print out the result.
     resp.on('end', () => {
       res.write('<html><head></head><body>');
-      res.write('<p>' + JSON.parse(data).explanation + '</p>');
+      res.write('<p>Hi Team' + JSON.parse(data).explanation + '</p>');
       res.end('</body></html>');
     });
 
